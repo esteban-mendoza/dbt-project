a4ba095eb5bcf237ce9eaf0c6b5d6e8033a2ed0f
Jorge Esteban M <jorge.mendoza@discordapp.com>
2023-10-04 19:33:33 -0600

    Added macros and used dbt_utils to generate a surrogate key

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -1,7 +1,12 @@
 {{ config(materialized="incremental", on_schema_change="fail") }}
 
 with src_reviews as (select * from {{ ref("src_reviews") }})
-select *
+select
+    {{
+        dbt_utils.generate_surrogate_key(
+            ["listing_id", "review_date", "reviewer_name", "review_text"]
+        )
+    }} as review_id, *
 from src_reviews
 where
     review_text is not null
